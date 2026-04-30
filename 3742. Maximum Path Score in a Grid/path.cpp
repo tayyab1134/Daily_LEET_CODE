f3a5db52dea10dc3dad347928aed915193f09b4f
@Tayyab Ali <tayyabasif029@gmail.com>
2026-04-30 09:21:48 +0500

    daily

---
+++ 3742. Maximum Path Score in a Grid/path.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k + 1, -1)));
+        
+        dp[0][0][0] = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int c = 0; c <= k; c++) {
+                    if (dp[i][j][c] == -1) continue;
+                    
+                    if (j + 1 < n) {
+                        int val = grid[i][j + 1];
+                        int cost = (val == 0 ? 0 : 1);
+                        if (c + cost <= k) {
+                            dp[i][j + 1][c + cost] = max(dp[i][j + 1][c + cost], dp[i][j][c] + val);
+                        }
+                    }
+                    
+                    if (i + 1 < m) {
+                        int val = grid[i + 1][j];
+                        int cost = (val == 0 ? 0 : 1);
+                        if (c + cost <= k) {
+                            dp[i + 1][j][c + cost] = max(dp[i + 1][j][c + cost], dp[i][j][c] + val);
+                        }
+                    }
+                }
+            }
+        }
+        
+        int result = -1;
+        for (int c = 0; c <= k; c++) {
+            result = max(result, dp[m - 1][n - 1][c]);
+        }
+        
+        return result;
+    }
+};
